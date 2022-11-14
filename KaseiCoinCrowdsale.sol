8397b4de97aad7557557f3fdc516cff7aa0ad939
kmpotter2 <97268901+kmpotter2@users.noreply.github.com>
2022-11-13 18:18:07 -0800

    Update KaseiCoinCrowdsale.sol
    Added ";" at the end of variable call outs

--- KaseiCoinCrowdsale.sol
+++ KaseiCoinCrowdsale.sol
@@ -23,9 +23,9 @@ contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale { // UPDATE THE CONTRA
 
 contract KaseiCoinCrowdsaleDeployer {
     // Create an `address public` variable called `kasei_token_address`.
-    address public kasei_token_address
+    address public kasei_token_address;
     // Create an `address public` variable called `kasei_crowdsale_address`.
-    address public kasei_crowdsale_address
+    address public kasei_crowdsale_address;
 
     // Add the constructor.
     constructor(
